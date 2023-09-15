9740c0b1a0ddf44148fb8cadf8557312c38042e1
Mks <parkersfly@gmail.com>
2023-09-15 16:24:33 -0300

    back-end integration completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import { Github, FileVideo, Upload, Wand2 } from "lucide-react"
+import { Github, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
+import { useCompletion } from 'ai/react'
 
 export function App() {
+  const [ temperature, setTemperature] = useState(0.5)
+  const [ videoId, setVideoId ] = useState<String | null>(null)
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/generate',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -17,10 +48,10 @@ export function App() {
             Desenvolvido com ❤ no NLW da Rocketseat
           </span>
 
-          <Separator orientation="vertical" className="h-6"/>
+          <Separator orientation="vertical" className="h-6" />
 
           <Button variant="outline">
-            <Github className="w-4 h-4 mr-2"/>
+            <Github className="w-4 h-4 mr-2" />
             Github
           </Button>
         </div>
@@ -29,72 +60,41 @@ export function App() {
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
-            <Textarea 
-            placeholder="Inclua o prompt para a IA..."
-            className="resize-none p-4 leading-relaxed"     
+            <Textarea
+              placeholder="Inclua o prompt para a IA..."
+              className="resize-none p-4 leading-relaxed"
+              value={input}
+              onChange={handleInputChange}
             />
-            <Textarea 
-            placeholder="Resultado gerado pela IA..." 
-            readOnly
-            className="resize-none p-4 leading-relaxed"
+            <Textarea
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+              className="resize-none p-4 leading-relaxed"
+              value={completion}
             />
           </div>
 
           <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
           </p>
         </div>
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label 
-            htmlFor="video"
-            className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4"/>
-              Selecione um vídeo
-            </label>
-
-            <input type="file" id="video" accept="video/mp4" className="sr-only"/>
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea 
-              id="transcription_prompt" 
-              className="h-20 leading-relaxed resize-none"
-              placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="w-4 h-4 ml-2"/>
-            </Button>
-          </form>
+          <VideoInputForm onVideoUploaded={setVideoId}/>
 
           <Separator />
 
-          <form className="space-y-6">
-
-          <div className="space-y-2">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput}/>
             </div>
 
             <div className="space-y-2">
               <Label>Modelo</Label>
-              <Select disabled defaultValue='gpt3.5'>
+              <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
@@ -111,25 +111,28 @@ export function App() {
 
             <div className="space-y-4">
               <Label>Temperatura</Label>
-              <Slider
-                min={0}
-                max={1}
-                step={0.1}
+              <Slider 
+              min={0} 
+              max={1} 
+              step={0.1} 
+              value={[temperature]}
+              onValueChange={ value => setTemperature(value[0])}
               />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
-                Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
               </span>
             </div>
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
-              <Wand2 className="w-4 h-4 ml-2"/>
+              <Wand2 className="w-4 h-4 ml-2" />
             </Button>
           </form>
         </aside>
       </main>
     </div>
-  )
+  );
 }
---
+++ src/components/prompt-select.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "./ui/select";
+import { api } from "@/lib/axios";
+
+interface Prompt {
+  id: string
+  name: string
+  template: string
+}
+
+interface PromptSelectProps {
+  onPromptSelected: (template: string) => void
+}
+
+export function PromptSelect(props: PromptSelectProps){
+  const [prompts, setPrompts] = useState<Prompt[] | null>(null)
+
+  function handlePromptSelected(promptId: string){
+    const selectedPrompt = prompts?.find(prompt => prompt.id === promptId)
+
+    if(!selectedPrompt){
+      return 
+    }
+
+    props.onPromptSelected(selectedPrompt.template)
+  }
+
+  useEffect(() => {
+    api.get('/prompts').then(response => {
+      setPrompts(response.data)
+    })
+  }, [])
+
+  return(
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..."/>
+      </SelectTrigger>
+      <SelectContent>
+        {prompts?.map(prompt => {
+            return (<SelectItem key={prompt.id} value={prompt.id}>
+              {prompt.name}
+            </SelectItem>)
+        })}
+      </SelectContent>
+    </Select>
+  )
+}
